7edab1c48e6b562932c963e9b5e74ff69dc86e21
Nino Jacobs <31828830+NinoJacobs@users.noreply.github.com>
2024-10-30 22:24:36 +0200

    ignore mappings for user

--- src/main/java/com/ratethisthing/ratethisthing/mappers/UserMapper.java
+++ src/main/java/com/ratethisthing/ratethisthing/mappers/UserMapper.java
@@ -3,6 +3,7 @@ package com.ratethisthing.ratethisthing.mappers;
 import com.ratethisthing.ratethisthing.entity.User;
 import com.ratethisthing.ratethisthing.openapi.ratethisthing.dto.UserDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -12,7 +13,8 @@ public interface UserMapper {
 
     UserDTO userEntityToDto(User user);
 
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     User userDtoToEntity(UserDTO userDto);
 }
-
-
