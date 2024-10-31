ace50a36aa3c8a9323d8dc4cf39fe475a003bcd7
Nino Jacobs <31828830+NinoJacobs@users.noreply.github.com>
2024-10-31 23:04:00 +0200

    implement item api

--- src/main/java/com/ratethisthing/ratethisthing/repository/ItemRepository.java
+++ src/main/java/com/ratethisthing/ratethisthing/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.ratethisthing.ratethisthing.repository;
 
 import com.ratethisthing.ratethisthing.entity.Item;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,5 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @EntityGraph(value = "Item.category", type = EntityGraph.EntityGraphType.LOAD)
-    Optional<Item> findById(Long id);
+    Optional<Item> findById(@NonNull Long id);
 }
